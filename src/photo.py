c5db2e98b8fb3369ced8f2bdf9180b9670525a16
Veronika Macku <200037@vutbr.cz>
2021-11-08 12:33:10 +0100

    Now using module os for filepaths and new method Draw Limits.

--- src/photo.py
+++ src/photo.py
@@ -1,11 +1,14 @@
 import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
-import skimage
-from skimage import measure
+import os
+#from matplotlib import pyplot as plt
+#import skimage
+#from skimage import measure
 
-
-
+CUR_PATH = os.path.dirname(__file__)
+CUR_PATH_ONE_UP, _ = os.path.split(CUR_PATH)
+PATH_DATA = os.path.join(CUR_PATH_ONE_UP,'data')
+PATH_PROCESSED_DATA = os.path.join(PATH_DATA,'processed')
 
 # Points for image cropping
 TOP_LEFT = (630, 100)
@@ -24,7 +27,14 @@ class Photo:
         # Converts image to GRAY format, only one channel is maintained
         self.photo = cv.cvtColor(photo, cv.COLOR_BGR2GRAY)
 
+        self.BOTTLE_HEIGHT_in_mm = 195 #[mm]
+        self.BOTTLE_HEIGHT_in_pxs = 990 #[mm]
 
+        self.surface_height_in_mm = 0  #[mm]
+        self.surface_height_in_px = 0
+        self.bottle_top_in_px = 0
+        self.surface_lvl_in_px = 0
+        self.bottle_bottom_in_px = 0
     # ---------------------------------------------------------------------------------------
     # Magic method to display name of the photos in arrays
     def __repr__(self):
@@ -35,10 +45,10 @@ class Photo:
     # ---------------------------------------------------------------------------------------
     # Image is saved to given directory, path is relative to script src directory
     # Note that image is NOT SAVED when DIRECTORY DOES NOT EXISTS
-    def save(self, directory = '../data/processed/'):
-
+    def save(self, directory = PATH_PROCESSED_DATA):
+        
         print("Saved to: ", directory)
-        cv.imwrite(directory + str(self.name), self.photo)
+        cv.imwrite(os.path.join(directory, self.name), self.photo)
 
 
     # ---------------------------------------------------------------------------------------
@@ -155,17 +165,54 @@ class Photo:
                     down = (column, row)
                     break
 
-        delka_cele_flasky_v_milimetrech = 220 # UPRAVTE
-        pocet_pixelu_cele_flasky = 990
-        pixely_na_milimetry = delka_cele_flasky_v_milimetrech/pocet_pixelu_cele_flasky
-        delka_horni_pulky_flasky_v_pixlech =  up[1] - down[1]
-        delka_horni_pulky_flasky_v_milimetrech = delka_horni_pulky_flasky_v_pixlech*pixely_na_milimetry
-        hladina_v_milimetrech = delka_cele_flasky_v_milimetrech - delka_horni_pulky_flasky_v_milimetrech
+        bottle_length_in_mm = self.BOTTLE_HEIGHT_in_mm
+        bottle_num_pixels = self.BOTTLE_HEIGHT_in_pxs
+        pixels_to_mm = bottle_length_in_mm/bottle_num_pixels
+        length_upper_half_of_bottle_in_pixels =  up[1] - down[1]
+        length_upper_half_of_bottle_in_mm = length_upper_half_of_bottle_in_pixels*pixels_to_mm
+        surface_in_mm = bottle_length_in_mm - length_upper_half_of_bottle_in_mm
+
+        print('Surface [mm]:', surface_in_mm)
+        self.surface_height_in_mm = surface_in_mm
+        self.surface_height_in_px  = bottle_num_pixels - length_upper_half_of_bottle_in_pixels
+        self.bottle_top_in_px = down[1]
+        self.surface_lvl_in_px = up[1]
+        self.bottle_bottom_in_px = down[1] + bottle_num_pixels
         
-        print('hladina_v_milimetrech:', hladina_v_milimetrech)
 
         self.photo = cv.line(self.photo, (20, up[1]), (20, down[1]), (255, 255, 255), 1)
         self.photo = cv.line(self.photo, up, down, (255, 255, 255), 1)
+
+    def drawLimits(self, eval = "failed"):
+        self.photo = cv.cvtColor(self.photo, cv.COLOR_GRAY2RGB)
+
+        top = self.bottle_top_in_px
+        surf =  self.surface_lvl_in_px
+        bottom = self.bottle_bottom_in_px
+
+        #SURFACE LEVEL
+        #self.photo = cv.line(self.photo, (20, surf), (400, surf), (0, 255, 0), 2)   #horizontal
+        #self.photo = cv.line(self.photo, (20, surf), (20, bottom), (0, 255, 0), 2)   #vertical
+        self.photo = cv.rectangle(self.photo, (20, surf), (400, bottom), (0, 255, 0),2)
+
+        #UPPER AND LOWER BOUND
+        self.photo = cv.line(self.photo, (20, top), (400, top), (0, 0, 255), 2)
+        self.photo = cv.line(self.photo, (20, bottom), (400, bottom), (0, 0, 255), 2) 
+
+        font = cv.FONT_HERSHEY_SIMPLEX
+        
+        offx = 200
+        offy = 40
+        self.photo = cv.putText(self.photo, "Surface:", (offx, 900), font, 1, (0, 255, 0), 1, cv.LINE_AA)
+        self.photo = cv.putText(self.photo, f"{self.surface_height_in_px} px", (offx, 900 + offy), font, 1, (0, 255, 0), 1, cv.LINE_AA)
+        self.photo = cv.putText(self.photo, f"{np.round(self.surface_height_in_mm,2)} mm", (offx, 900 + offy*2), font, 1, (0, 255, 0), 1, cv.LINE_AA)
+
+        if eval == "failed":
+            self.photo = cv.putText(self.photo, eval, (120, 800), font, 2, (0, 0, 255), 2, cv.LINE_AA)
+        else:
+            self.photo = cv.putText(self.photo, eval, (120, 800), font, 2, (0, 255, 0), 2, cv.LINE_AA)
+
+
 
 
 
@@ -176,7 +223,8 @@ class Photo:
 if __name__ == "__main__":
 
     # Load the image from directory
-    img = cv.imread("../data/flaska95.png")
+    PHOTO_PATH = os.path.join(PATH_DATA,"flaska107.png")
+    img = cv.imread(PHOTO_PATH)
 
     # Create instance of class Photo
     p = Photo("bottle", img)
